a86f6c0e9cf0f1787ee193e694eb9efdd312f217
Suhas Venugopal <suhasvenugopal@Suhass-MacBook-Pro.local>
2024-12-07 15:56:45 +0530

    added opeartors chapter

---
+++ 01-Fundamentals-Part-1/04-operators/script.js
@@ -0,0 +1,34 @@
+/* = is as assignment operator */
+
+let a = 10;
+
+console.log(a);
+
+/* this transaltes to 
+   a = a + 10;
+*/
+
+a += 10; 
+
+/* this is post increment
+   the value of a is used and
+   then incremented
+*/
+
+console.log(a++); // This prints 20
+
+/* this is pre increment
+   the value of a is incremented and
+   then used
+*/
+
+console.log(++a); // This prints 22
+
+let b = 10;
+
+/* ** is power opeartor */
+
+console.log( b**2);
+
+console.log( b**3);
+
