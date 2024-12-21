c472a15678f0abb86da8d4edc1ee576abcadf554
Suhas Venugopal <suhasvenugopal@Suhass-MacBook-Pro.local>
2024-12-21 14:55:11 +0530

    added primeNumber functions

--- 02-Fundamentals-Part-2/02-functions/script.js
+++ 02-Fundamentals-Part-2/02-functions/script.js
@@ -57,6 +57,35 @@ let limit = Number(prompt(`Enter the Limit till you want the prime number`));
 alert(primeNumberTill(limit));
 
 const nthPrimeNumber = function (n){
-    
+
+    let primeArray = [];
+
+    for(let i=0;i>-1;i++){
+
+        let flag = false;
+
+        for(let j=2;j<i;j++){
+
+            if(i%j==0){
+                flag = true;
+            }
+        }
+
+        if(!flag){
+            primeArray.push(i);
+        }
+
+        if(primeArray.length == n){
+
+            break;
+        }
+    }
+
+    return `The ${n}th prime number is ${primeArray[primeArray.length-1]} and here is the array ${primeArray}`;
+
 }
+
+let nOfNthPrime = Number(prompt(`Enter the nth number of which prime is expected`));
+
+alert(nthPrimeNumber(nOfNthPrime));
 
