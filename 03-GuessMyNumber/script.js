134278ff606d822e95f49e91123e94ce6f2b37b1
Suhas Venugopal <suhasvenugopal@Suhass-MacBook-Pro.local>
2025-01-08 17:39:02 +0530

    modified the guess my number game

--- 03-GuessMyNumber/script.js
+++ 03-GuessMyNumber/script.js
@@ -25,7 +25,7 @@ document.querySelector('.message').textContent = 'Some new value';
  * to select input element
  */
 
-let inputElement=document.querySelector('section.left>input.guess');
+let inputElement = document.querySelector('section.left>input.guess');
 
 console.log(inputElement);
 
@@ -47,42 +47,96 @@ inputElement.value = 10;
 
 inputElement.value = null;
 
-const innerFunction = function(){
+const innerFunction = function () {
     console.log(inputElement.value);
 }
 
+let score = 20;
 
-document.querySelector('.btn.check').addEventListener('click', function(){
+let highScore = 0;
+
+let number = Math.round(Math.random() * 20);
+
+if (!number) number = 1;
+
+console.log(number);
+
+document.querySelector('.number').textContent = '?';
+
+document.querySelector('.btn.check').addEventListener('click', function () {
 
     console.log(inputElement.value);
 
     /**
      * This is an async logging dont know what is the use
      */
-    actualLog(inputElement.value);   
-    
+    actualLog(inputElement.value);
+
     const guess = Number(inputElement.value);
 
-    if(!guess){
-        document.querySelector('.message').textContent = 'No Number!!!'
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No Number!!!';
+    } else if (guess === number) {
+        document.querySelector('.message').textContent = 'Correct Number!!!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem'
+        document.querySelector('.number').textContent = number;
+        if(score>highScore){
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+    } else if (guess > number) {
+        if(score>1){
+            document.querySelector('.message').textContent = 'Too High!!!';
+            document.querySelector('.score').textContent = --score;
+        } else {
+            document.querySelector('.message').textContent = 'You Lost the Game!!!';
+            document.querySelector('.score').textContent = 0;
+        }
+        
+    } else if (guess < number) {
+        if(score>1){
+            document.querySelector('.message').textContent = 'Too Low!!!';
+            document.querySelector('.score').textContent = --score;
+        } else {
+            document.querySelector('.message').textContent = 'You Lost the Game!!!';
+            document.querySelector('.score').textContent = 0;
+        }
+        
     }
 
-    // some other change
-
-    // one more comment
-
-    //  to fix the issues
-    
 });
 
-const logger = (async (something)=>{
+document.querySelector('.again').addEventListener('click',function(){
+
+    number = Math.round(Math.random() * 20);
+
+    if (!number) number = 1;
+
+    console.log(number);
+
+    score = 20;
+
+    document.querySelector('.guess').value = null;
+
+    document.querySelector('.message').textContent = 'Start guessing...';
+
+    document.querySelector('.number').textContent = '?';
+
+    document.querySelector('body').style.backgroundColor= '#222';
+
+    document.querySelector('.number').style.width = '15rem';
+
+})
+
+const logger = (async (something) => {
 
     console.log(something);
 });
 
 
 
-const actualLog = (async(something)=>{
+const actualLog = (async (something) => {
 
     await logger(something);
 });
